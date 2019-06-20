63dce474926d9b1b4cb9c5446e890e4ea8aa5a49
DLMousey <tom@deadlyllama.com>
2019-06-20 23:32:22 +0100

    Add a controller method that returns a view

--- src/main/java/com/enderstudy/controller/DemoController.java
+++ src/main/java/com/enderstudy/controller/DemoController.java
@@ -12,4 +12,11 @@ public class DemoController {
     public String hello() {
         return "Hello";
     }
+
+    // Leading slash will be added automatically, ResponseBody annotation not required.
+    // View should be automatically resolved due to file name matching method name
+    @GetMapping("welcome")
+    public String welcome() {
+        return "welcome";
+    }
 }
